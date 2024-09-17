a71636cd2472c8e5cf6ba1240fdc081ef3671899
Reynaldo Rodrigo Choque Vicente <reynaldorodrigochoque@gmail.com>
2024-09-17 09:09:14 -0400

    Se cambio la estructura de los enlaces del menú

--- js/index.js
+++ js/index.js
@@ -1,7 +1,12 @@
 const body = document.querySelector("body"),
-  modeSwitch = body.querySelector(".mode-switch");
+  modeSwitch = body.querySelector(".mode-switch"),
+  menuItems = body.querySelector(".menu-items li");
 
 modeSwitch.addEventListener("click", () => {
   body.classList.toggle("dark");
   console.log("dark");
+});
+
+menuItems.addEventListener("click", () => {
+  menuItems.classList.toggle("open");
 });
