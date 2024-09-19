d84971c41eb0f5cdee86727b6bbcf900ca476a20
Reynaldo Rodrigo Choque Vicente <reynaldorodrigochoque@gmail.com>
2024-09-19 16:15:30 -0400

    Se ajustó los estilos del submenú

--- js/index.js
+++ js/index.js
@@ -1,12 +1,44 @@
 const body = document.querySelector("body"),
   modeSwitch = body.querySelector(".mode-switch"),
-  menuItems = body.querySelector(".menu-items li");
+  menuItems = body.querySelectorAll(".menu-link");
 
 modeSwitch.addEventListener("click", () => {
   body.classList.toggle("dark");
   console.log("dark");
 });
 
-menuItems.addEventListener("click", () => {
-  menuItems.classList.toggle("open");
+// menuItems.addEventListener("click", () => {
+//   menuItems.classList.toggle("open");
+// });
+
+//const menuItems = document.querySelectorAll('.menu-item'); // Selecciona todos los elementos del menú
+
+menuItems.forEach((menuItem) => {
+  if (menuItem.classList.contains("collapsed")) {
+    menuItem.addEventListener("click", (event) => {
+      event.preventDefault(); // Previene el comportamiento por defecto del enlace
+      const isOpen = menuItem.classList.contains("open");
+      // Cierra todos los submenús
+      menuItems.forEach((item) => item.classList.remove("open"));
+      // Abre el submenú del menú seleccionado
+      if (!isOpen) {
+        menuItem.classList.add("open");
+      }
+    });
+  }
+
+  // const menuLink = menuItem.querySelector('.collapsed');
+
+  // menuLink.addEventListener('click', (event) => {
+  //     event.preventDefault(); // Previene el comportamiento por defecto del enlace
+  //     const isOpen = menuItem.classList.contains('open');
+
+  //     // Cierra todos los submenús
+  //     menuItems.forEach(item => item.classList.remove('open'));
+
+  //     // Abre el submenú del menú seleccionado
+  //     if (!isOpen) {
+  //         menuItem.classList.add('open');
+  //     }
+  // });
 });
